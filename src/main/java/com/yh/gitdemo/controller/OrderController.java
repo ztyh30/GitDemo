d462c1770c16c838bd80272069285849c33de96e
ztyh <997475779@qq.com>
2022-09-16 14:03:10 +0800

    导入List包

--- src/main/java/com/yh/gitdemo/controller/OrderController.java
+++ src/main/java/com/yh/gitdemo/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.yh.gitdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class OrderController {
 
